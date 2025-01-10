e4413ff5905d3bfcce9d4e13410f31172f855f37
ivazhnik <vazhnikiu@gmail.com>
2025-01-10 11:02:44 +0300

    Homework_3 done

---
+++ homework/igor_vazhnik/Homework_3/task1.py
@@ -0,0 +1,13 @@
+num1 = 3
+num2 = 2
+
+print('sum num1 and num2: ')
+print(num1 + num2)
+print()
+print('difference num1 and num2')
+print(num1 - num2)
+print()
+print('multiplication num1 and num2')
+print(num1 * num2)
+
+
---
+++ homework/igor_vazhnik/Lesson_3/lesson.py
@@ -0,0 +1,24 @@
+# Идентификаторы
+
+print(42)
+print(3.5)
+print('Strin')
+print(3 == 3)
+print(3 != 3)
+print()
+print(3 >= 3)
+print(3 <= 3)
+
+var = 123
+empty = ''
+
+#print(3 + var)
+
+b = None
+
+print(b)
+
+if 1 < 2 and 1 > 0:
+    print('Good evening!')
+
+print('E' in 'hello')
