d518c8de5aeb01257ee29c1e595876dd7e497901
ivazhnik <vazhnikiu@gmail.com>
2025-01-27 18:47:43 +0300

    fixed task2, task3 homework_5

--- homework/igor_vazhnik/Homework_5/task3.py
+++ homework/igor_vazhnik/Homework_5/task3.py
@@ -2,4 +2,4 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 student, student2, student3 = students
 subject, subject2, subject3 = subjects
-print('Students', ', '.join(students),'study these subjects:', ', '.join(subjects))
+print('Students', ', '.join(students), 'study these subjects:', ', '.join(subjects))
