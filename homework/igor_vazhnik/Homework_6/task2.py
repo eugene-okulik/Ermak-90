cd0db7f83c5dd1ead524fc26fabf875880d70849
ivazhnik <vazhnikiu@gmail.com>
2025-01-29 20:13:39 +0300

    fixed formatting

--- homework/igor_vazhnik/Homework_6/task2.py
+++ homework/igor_vazhnik/Homework_6/task2.py
@@ -1,7 +1,7 @@
 ran = range(1, 101)
 for number in ran:
     if number % 3 == 0 and number % 5 == 0:
-      print('FuzzBuzz')
+        print('FuzzBuzz')
     elif number % 3 == 0:
         print('Fuzz')
     elif number % 5 == 0:
