aa7c0cd4d0447e87997c65e135a56ec23480678d
ivazhnik <vazhnikiu@gmail.com>
2025-02-02 14:20:53 +0300

    fixed validation

--- homework/igor_vazhnik/Homework_7/task2.py
+++ homework/igor_vazhnik/Homework_7/task2.py
@@ -1,7 +1,7 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
-def result(dict):
-    for key, value in dict.items():
+def result(diction):
+    for key, value in diction.items():
         for i in range(value):
             print(key, end="")
         print()
