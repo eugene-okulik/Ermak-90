f441265d440ac658643dc4fd33c424e619e7b127
ivazhnik <vazhnikiu@gmail.com>
2025-02-16 13:34:05 +0300

    Fixed validation task4.py

--- homework/igor_vazhnik/Homework_10/task4.py
+++ homework/igor_vazhnik/Homework_10/task4.py
@@ -13,8 +13,8 @@ print(PRICE_LIST)
 #               for element in PRICE_LIST.split('\n')))
 
 res_dict = dict((a, int(b.strip('р')))
-    for a, b in (element.split()
-        for element in PRICE_LIST.split('\n')))
+                for a, b in (element.split()
+                             for element in PRICE_LIST.split('\n')))
 
 print(res_dict)
 # {'тетрадь': 50, 'книга': 200, 'ручка': 100, 'карандаш': 70, 'альбом': 120, 'пенал': 300, 'рюкзак': 500}
