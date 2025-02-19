0f6ffcf9f86ccfe3b4ccdc0faa398b4477e69585
ivazhnik <vazhnikiu@gmail.com>
2025-02-19 10:30:01 +0300

    Added methods for classes

--- homework/igor_vazhnik/Homework_11/task.py
+++ homework/igor_vazhnik/Homework_11/task.py
@@ -10,6 +10,15 @@ class Book:
         self.reserved = reserved
 
 
+    def printed(self):
+        if self.reserved:
+            print(
+                f'Название: {self.title}, Автор: {self.author}, страниц: {self.number_page}, материал: {self.page_material},'
+                f' зарезервирована')
+        else:
+            print(f'Название: {self.title}, Автор: {self.author}, страниц: {self.number_page}, материал: {self.page_material}')
+
+
 book = Book('Идиот', 'Достоевский', 501, 'asdf', False)
 book2 = Book('Идиот2', 'Достоевский', 502, 'asdf', True)
 book3 = Book('Идиот3', 'Достоевский', 503, 'asdf', False)
@@ -18,40 +27,11 @@ book5 = Book('Идиот5', 'Достоевский', 505, 'asdf', False)
 
 book.reserved = True
 
-if book.reserved:
-    print(f'Название: {book.title}, Автор: {book.author}, страниц: {book.number_page}, материал: {book.page_material},'
-          f' зарезервирована')
-else:
-    print(f'Название: {book.title}, Автор: {book.author}, страниц: {book.number_page}, материал: {book.page_material}')
-# print(book)
-
-if book2.reserved:
-    print(f'Название: {book2.title}, Автор: {book2.author}, страниц: {book2.number_page}, материал: '
-          f'{book2.page_material}, зарезервирована')
-else:
-    print(f'Название: {book2.title}, Автор: {book2.author}, страниц: {book2.number_page},  материал: '
-          f'{book2.page_material}')
-
-if book3.reserved:
-    print(f'Название: {book3.title}, Автор: {book3.author}, страниц: {book3.number_page}, материал: '
-          f'{book3.page_material}, зарезервирована')
-else:
-    print(f'Название: {book3.title}, Автор: {book3.author}, страниц: {book3.number_page},  материал: '
-          f'{book3.page_material}')
-
-if book4.reserved:
-    print(f'Название: {book4.title}, Автор: {book4.author}, страниц: {book4.number_page}, материал: '
-          f'{book4.page_material}, зарезервирована')
-else:
-    print(f'Название: {book4.title}, Автор: {book4.author}, страниц: {book4.number_page},  материал: '
-          f'{book4.page_material}')
-
-if book5.reserved:
-    print(f'Название: {book5.title}, Автор: {book5.author}, страниц: {book5.number_page}, материал: '
-          f'{book5.page_material}, зарезервирована')
-else:
-    print(f'Название: {book5.title}, Автор: {book5.author}, страниц: {book5.number_page},  материал: '
-          f'{book5.page_material}')
+book.printed()
+book2.printed()
+book3.printed()
+book4.printed()
+book5.printed()
 
 
 class SchoolTextbooks(Book):
@@ -63,22 +43,19 @@ class SchoolTextbooks(Book):
         self.availability_tasks = availability_tasks
 
 
+    def printed(self):
+        if self.reserved:
+            print(f'Название: {self.title}, Автор: {self.author}, страниц: {self.number_page}, предмет: '
+          f'{self.school_subject}, класс: {self.school_class}, зарезервирована')
+        else:
+            print(f'Название: {self.title}, Автор: {self.author}, страниц: {self.number_page}, предмет: '
+                  f'{self.school_subject}, класс: {self.school_class}')
+
+
 textBooks = SchoolTextbooks('Математика', 'Иванов', 501, 'asdf', False,
                             'Алгебра', 9, False)
 textBooks2 = SchoolTextbooks('Химия', 'Иванов', 501, 'asdf', False,
                              'Химия', 9, False)
 textBooks.reserved = True
-
-if textBooks.reserved:
-    print(f'Название: {textBooks.title}, Автор: {textBooks.author}, страниц: {textBooks.number_page}, предмет: '
-          f'{textBooks.school_subject}, класс: {textBooks.school_class}, зарезервирована')
-else:
-    print(f'Название: {textBooks.title}, Автор: {textBooks.author}, страниц: {textBooks.number_page}, предмет: '
-          f'{textBooks.school_subject}, класс: {textBooks.school_class}')
-
-if textBooks2.reserved:
-    print(f'Название: {textBooks2.title}, Автор: {textBooks2.author}, страниц: {textBooks2.number_page}, предмет: '
-          f'{textBooks2.school_subject}, класс: {textBooks2.school_class}, зарезервирована')
-else:
-    print(f'Название: {textBooks2.title}, Автор: {textBooks2.author}, страниц: {textBooks2.number_page}, предмет: '
-          f'{textBooks2.school_subject}, класс: {textBooks2.school_class}')
+textBooks.printed()
+textBooks2.printed()
